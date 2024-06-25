1923290808cfce851c13fde9dc29ec376e5954e9
Sergei Markov <smarkov.dev@gmail.com>
2024-06-25 23:21:01 +0300

    Project init

---
+++ Ducki2Client/Duckie2Client/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Duckie2Client.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+#pragma warning disable CA1822 // Mark members as static
+    public string Greeting => "Welcome to Avalonia!";
+#pragma warning restore CA1822 // Mark members as static
+}
