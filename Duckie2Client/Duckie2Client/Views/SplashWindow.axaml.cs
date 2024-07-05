9ce01ac246378bb861f4edb066cf2f402e8cd425
Sergei Markov <smarkov.dev@gmail.com>
2024-07-05 19:30:59 +0300

    Graphical design of the Splash Screen.

--- Duckie2Client/Duckie2Client/Views/SplashWindow.axaml.cs
+++ Duckie2Client/Duckie2Client/Views/SplashWindow.axaml.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Threading.Tasks;
+using Avalonia.Animation;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Styling;
 using Avalonia.Threading;
 using Duckie2Client.Libs;
 using Duckie2Client.Services;
@@ -19,8 +21,11 @@ public partial class SplashWindow : Window
     }
     protected override void OnLoaded(RoutedEventArgs routedEventArgs)
     {
+        // StartAnimation();
+        
         // Begin the Application loading.
         DuckieLoad();
+        
     }
 
     private async void DuckieLoad()
@@ -37,10 +42,46 @@ public partial class SplashWindow : Window
         StatusMessage.Text = "All done";
         await Task.Delay(1500);
         
+        return; 
+        
         await Dispatcher.UIThread.InvokeAsync(() =>
         {
             _mainAction?.Invoke();
             Close();
         });
+    }
+    
+    private void StartAnimation()
+    {
+        var movingImage = this.FindControl<Image>("movingImage");
+
+        var animation = new Animation
+        {
+            Duration = TimeSpan.FromSeconds(5),
+            Easing = new Avalonia.Animation.Easings.CubicEaseInOut(),
+            Children =
+            {
+                new KeyFrame
+                {
+                    Cue = new Cue(0d),
+                    Setters =
+                    {
+                        new Setter(Canvas.LeftProperty, 0d),
+                        new Setter(Canvas.TopProperty, 0d)
+                    }
+                },
+                new KeyFrame
+                {
+                    Cue = new Cue(1d),
+                    Setters =
+                    {
+                        new Setter(Canvas.LeftProperty, 400d),
+                        new Setter(Canvas.TopProperty, 300d)
+                    }
+                }
+            }
+        };
+
+        animation.RunAsync(movingImage);
     }
 }
