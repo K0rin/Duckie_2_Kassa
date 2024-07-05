21c9343ad761965ea6f0f795fb0841351efce567
Sergei Markov <smarkov.dev@gmail.com>
2024-07-05 10:07:13 +0300

    ver1

---
+++ Duckie2Client/Duckie2Client/App.axaml.cs
@@ -0,0 +1,37 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using Duckie2Client.ViewModels;
+using Duckie2Client.Views;
+
+namespace Duckie2Client;
+
+public partial class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.MainWindow = new SplashWindow(() =>
+            {
+                
+                var mainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel()
+                };
+
+                mainWindow.Show();
+                mainWindow.Focus();
+                
+                desktop.MainWindow = mainWindow;
+            });
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
---
+++ Duckie2Client/Duckie2Client/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Duckie2Client.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
