1127d3c0aae8c8d256289ffd83ee81467005dc5b
Sergei Markov <smarkov.dev@gmail.com>
2024-08-09 09:29:30 +0300

    Cleanup

--- Duckie2Client/Duckie2Client/ViewModels/ConsoleWindowViewModel.cs
+++ Duckie2Client/Duckie2Client/ViewModels/ConsoleWindowViewModel.cs
@@ -28,65 +28,7 @@ public class ConsoleWindowViewModel : ViewModelBase
 
     public ConsoleWindowViewModel()
     {
-        // Set current page to first on start up
-        _currentPage = _pages[0];
-
-
         // By default, the Authorization Screen is visible.
-        // IsAuthorizationScreenVisible = true;
-        // By default, the spinner is hidden.
-        // IsSpinnerVisible = false;
-        // IsAuthControlsVisible = true;
-
-        // OpenMainScreenCommand = ReactiveCommand.Create<UserControl>(OpenMainScreen);
+        _currentPage = _pages[0];
     }
-
-
-    // private void OpenMainScreen(object screen)
-    private void OpenMainScreen()
-    {
-        CurrentPage = _pages[1];
-
-        // IsAuthControlsVisible = false;
-        // IsSpinnerVisible = true;
-
-        // MainWindowContent = screen;
-        // IsAuthorizationScreenVisible = false;
-    }
-
-    // public ReactiveCommand<UserControl, Unit> OpenMainScreenCommand { get; }
-
-    // private object _mainWindowContent;
-    //
-    // public object MainWindowContent
-    // {
-    //     get => _mainWindowContent;
-    //     set => this.RaiseAndSetIfChanged(ref _mainWindowContent, value);
-    // }
-
-    // private bool _isAuthorizationScreenVisible;
-
-    // public bool IsAuthorizationScreenVisible
-    // {
-    //     get => _isAuthorizationScreenVisible;
-    //     set => this.RaiseAndSetIfChanged(
-    //         ref _isAuthorizationScreenVisible,
-    //         value);
-    // }
-
-    // private bool _isSpinnerVisible;
-    //
-    // public bool IsSpinnerVisible
-    // {
-    //     get => _isSpinnerVisible;
-    //     set => this.RaiseAndSetIfChanged(ref _isSpinnerVisible, value);
-    // }
-    //
-    // private bool _isAuthControlsVisible;
-    //
-    // public bool IsAuthControlsVisible
-    // {
-    //     get => _isAuthControlsVisible;
-    //     set => this.RaiseAndSetIfChanged(ref _isAuthControlsVisible, value);
-    // }
 }
