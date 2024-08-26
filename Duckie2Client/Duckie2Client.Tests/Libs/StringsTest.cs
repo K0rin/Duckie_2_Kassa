b7fcf90fbfae8cd31575fad2a2bb358d7e4e1e69
Sergei Markov <smarkov.dev@gmail.com>
2024-08-26 13:40:29 +0300

    tests

---
+++ Duckie2Client/Duckie2Client.Tests/Libs/StringsTest.cs
@@ -0,0 +1,18 @@
+﻿using Duckie2Client.Libs;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace Duckie2Client.Tests.Libs;
+
+[TestSubject(typeof(Strings))]
+public class StringsTest
+{
+    [Fact]
+    public void GetFirstTitleCaseTest()
+    {
+        const string INITIAL_STRING = "lower_case_string";
+        const string EXPECTED = "Lower_case_string";
+        var result = Strings.GetFirstTitleCase(INITIAL_STRING);
+        Assert.Equal(EXPECTED, result);
+    }
+}
