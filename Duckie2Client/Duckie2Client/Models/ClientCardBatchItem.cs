2c51fdb3df95cc742b8be60d0b60a643be35ddfd
Sergei Markov <smarkov.dev@gmail.com>
2024-09-09 18:47:26 +0300

    Preparation of a client list for adding into database...

---
+++ Duckie2Client/Duckie2Client/Models/ClientCardBatchItem.cs
@@ -0,0 +1,26 @@
+﻿using ReactiveUI;
+
+namespace Duckie2Client.Models;
+
+public class ClientCardBatchItem : ReactiveObject
+{
+    public ClientCardBatchItem(string? clientFirstName, string? clientLastName, string? clientPhone,
+        string? companyName, string? vehicleLicence, string? vehiclePriceCategory, int? vehicleDiscount)
+    {
+        ClientFirstName = clientFirstName;
+        ClientLastName = clientLastName;
+        ClientPhone = clientPhone;
+        CompanyName = companyName;
+        VehicleLicence = vehicleLicence;
+        VehiclePriceCategory = vehiclePriceCategory;
+        VehicleDiscount = vehicleDiscount;
+    }
+
+    public string? ClientFirstName { get; set; }
+    public string? ClientLastName { get; set; }
+    public string? ClientPhone { get; set; }
+    public string? CompanyName { get; set; }
+    public string? VehicleLicence { get; set; }
+    public string? VehiclePriceCategory { get; set; }
+    public int? VehicleDiscount { get; set; }
+}
--- Duckie2Client/Duckie2Client/ViewModels/Screens/ManagerConsole/ClientCardBatchAddScreen/ClientCardBatchAddScreenViewModel.cs
+++ Duckie2Client/Duckie2Client/ViewModels/Screens/ManagerConsole/ClientCardBatchAddScreen/ClientCardBatchAddScreenViewModel.cs
@@ -13,22 +13,60 @@ namespace Duckie2Client.ViewModels.Screens.ManagerConsole.ClientCardBatchAddScre
 public class ClientCardBatchAddScreenViewModel : ViewModelBase, ITabViewModel<List<string>>
 {
     [Reactive] public List<string>? DataPayload { get; set; }
-
-    public ObservableCollection<RemovableListItem>? VehicleLicensesItems { get; set; }
-    [Reactive] public string VehicleLicenceTextBoxValue { get; set; }
+    public ObservableCollection<ClientCardBatchItem>? ClientClientCardBatchAddItems { get; set; }
     public ReactiveCommand<TextBox, Unit> AddVehicleLicenceCommand { get; }
 
     public ClientCardBatchAddScreenViewModel()
     {
         AddVehicleLicenceCommand = ReactiveCommand.Create<TextBox>(AddVehicleLicenceExecute);
-        VehicleLicensesItems = [];
+        ClientClientCardBatchAddItems = [];
     }
 
 
+    [Reactive] public string ClientFirstNameValue { get; set; }
+    [Reactive] public string ClientLastNameValue { get; set; }
+    [Reactive] public string ClientPhoneValue { get; set; }
+    [Reactive] public string CompanyNameValue { get; set; }
+    [Reactive] public string NewCompanyNameValue { get; set; }
+    [Reactive] public string VehicleLicenceValue { get; set; }
+    [Reactive] public object VehiclePriceCategoryValue { get; set; }
+    [Reactive] public string VehicleDiscountValue { get; set; }
+
+    public ObservableCollection<ClientCardBatchItem> ClientsToAdd { get; set; }
+    private readonly List<ClientCardBatchItem> _newClients = [];
+
     private void AddVehicleLicenceExecute(TextBox textBox)
     {
-        var newLicence = new RemovableListItem(VehicleLicenceTextBoxValue);
-        VehicleLicensesItems?.Add(newLicence);
+        var isDiscountSuccessfully = int.TryParse(VehicleDiscountValue, out var vehicleDiscount);
+        if (!isDiscountSuccessfully)
+        {
+            // todo: Show error message to user.
+            Console.WriteLine(@"Cannot convert the vehicle discount value.");
+            return;
+        }
+
+        // todo: Restriction to a discount value.
+
+        // todo: If there is a new firm name entered, do not treat the company name combobox.
+
+        // todo: change to DataGrid.
+        // https://docs.avaloniaui.net/docs/reference/controls/datagrid
+
+        var vehiclePriceCategory = ((ComboBoxItem)VehiclePriceCategoryValue).Content as string;
+
+        var newItem = new ClientCardBatchItem(
+            ClientFirstNameValue,
+            ClientLastNameValue,
+            ClientPhoneValue,
+            CompanyNameValue,
+            VehicleLicenceValue,
+            vehiclePriceCategory,
+            vehicleDiscount);
+
+        _newClients.Add(newItem);
+        // ClientClientCardBatchAddItems?.Add(newLicence);
+
+        ClientsToAdd = new ObservableCollection<ClientCardBatchItem>(_newClients);
 
         // --- 
         textBox.Clear();
