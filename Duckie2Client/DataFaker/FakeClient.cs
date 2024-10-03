ff5640856b3d32be9c08f1f70a0aa7c665fdd6e4
Sergei Markov <smarkov.dev@gmail.com>
2024-10-03 14:18:31 +0300

    Data faker project added.

---
+++ Duckie2Client/DataFaker/FakeClient.cs
@@ -0,0 +1,24 @@
+﻿namespace DataFaker;
+
+public class FakeClient
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Notes { get; set; }
+    public DateTime RegistrationDateTime { get; set; }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public FakeBonus Bonus { get; set; }
+    public FakeVehicle Vehicle { get; set; }
+}
+
+public class FakeBonus
+{
+    public decimal Summa { get; set; }
+    public DateTime BonusEndDateTime { get; set; }
+}
+
+public class FakeVehicle
+{
+    public string? Licence { get; set; }
+}
